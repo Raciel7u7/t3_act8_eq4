69850054e0c682a569cbba78a8b4f75ca6c50224
LuisEduardoChavezLjx <218404691+LuisEduardoChavezLjx@users.noreply.github.com>
2026-07-14 10:08:42 -0600

    commit final agregar credenciales

--- src/utilidades/constantes.js
+++ src/utilidades/constantes.js
@@ -17,7 +17,27 @@ export const RUTA_LOGO_CLINICA = 'imagenes/logo-clinica.png';
 export const RUTA_ILUSTRACION_ACCESO = 'imagenes/ilustracion-acceso.png';
 
 /** Credenciales de prueba validas segun https://dummyjson.com/users */
-export const USUARIO_DEMOSTRACION = {
-  usuario: 'emilys',
-  contrasena: 'emilyspass',
-};
+export const USUARIOS_DEMOSTRACION = [
+  { usuario: 'emilys', contrasena: 'emilyspass' },
+  { usuario: 'michaelw', contrasena: 'michaelwpass' },
+  { usuario: 'sophiab', contrasena: 'sophiabpass' },
+  { usuario: 'jamesd', contrasena: 'jamesdpass' },
+  { usuario: 'emmaj', contrasena: 'emmajpass' },
+  { usuario: 'oliviaw', contrasena: 'oliviawpass' },
+  { usuario: 'alexanderj', contrasena: 'alexanderjpass' },
+  { usuario: 'avat', contrasena: 'avatpass' },
+  { usuario: 'ethanm', contrasena: 'ethanmpass' },
+  { usuario: 'isabellad', contrasena: 'isabelladpass' },
+  { usuario: 'liamg', contrasena: 'liamgpass' },
+  { usuario: 'miar', contrasena: 'miarpass' },
+  { usuario: 'noahh', contrasena: 'noahhpass' },
+  { usuario: 'charlottem', contrasena: 'charlottempass' },
+  { usuario: 'williamg', contrasena: 'williamgpass' },
+  { usuario: 'averyp', contrasena: 'averyppass' },
+  { usuario: 'evelyns', contrasena: 'evelynspass' },
+  { usuario: 'logant', contrasena: 'logantpass' },
+  { usuario: 'abigailr', contrasena: 'abigailrpass' },
+  { usuario: 'jacksone', contrasena: 'jacksonepass' },
+];
+
+export const USUARIO_DEMOSTRACION = USUARIOS_DEMOSTRACION[0];
